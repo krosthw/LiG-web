01d5217c316f20e27ac92b0dee6bf83b361d8251
krosthw <crosta93@gmail.com>
2023-10-07 10:49:43 +0200

    first navbar

---
+++ app/(route)/page.tsx
@@ -0,0 +1,8 @@
+const HomePage = () =>
+{
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default HomePage;
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-js text-black">
+                    &copy; 2023 LiG. All rights reseved.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
